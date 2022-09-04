a91bd6935259d23167e7d9a8704ea20820495eeb
Sean Wong <derpydm@gmail.com>
2022-09-04 16:19:50 +0800

    implement form validation for other API functions

--- src/server/handlers/api/auth/editUser.ts
+++ src/server/handlers/api/auth/editUser.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import messages from "../../../../static/messages.js";
 
 async function editUser(req: Request, res: Response) {
-    
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400);
+        res.send(messages.error.bad_request);
+    }
+
 }
 
 export default editUser;
