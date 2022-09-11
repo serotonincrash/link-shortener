cf62bef906d2e1968518f163215e576121f52068
Sean Wong <derpydm@gmail.com>
2022-09-11 17:12:37 +0800

    add checker middleware to ensure url doesnt exist

---
+++ src/server/handlers/helpers/url/denyIfURLExists.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import URL from "../../../../models/urlModel.js";
+import messages from "../../../../static/messages.js";
+
+async function denyIfURLExists(req: Request, res: Response, next: Function) {
+    // Checks whether user is logged in
+    let url = req.body.shortURL!
+
+    let foundURL = await URL.findOne({shortURL: url});
+
+    if (!foundURL) {
+        next();
+    } else {
+        res.status(403);
+        res.end(messages.permissions.url_already_exists);
+        return;
+    }
+}
+
+export default denyIfURLExists;
